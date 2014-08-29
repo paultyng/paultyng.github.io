a4115d9b37d8bd92b76a11a58532b27b39217c7a
Paul Tyng <paul@paultyng.net>
2014-08-28 22:28:25 -0400

    Update map

--- minecraft/overviewerConfig.js
+++ minecraft/overviewerConfig.js
@@ -10,7 +10,7 @@ var overviewerConfig = {
                 -22
             ], 
             "isOverlay": false, 
-            "last_rendertime": 1409199484, 
+            "last_rendertime": 1409279156, 
             "name": "Overviewer Render (lighting)", 
             "poititle": "Markers", 
             "north_direction": 0, 
@@ -60,6 +60,6 @@ var overviewerConfig = {
             67, 
             94
         ], 
-        "cacheTag": "1409229105"
+        "cacheTag": "1409279217"
     }
 };
