d1089277f7f695e44d4565d64810813bf5764b0c
Paul Tyng <paul@paultyng.net>
2014-09-06 02:14:11 -0400

    render

--- minecraft/overviewerConfig.js
+++ minecraft/overviewerConfig.js
@@ -10,7 +10,7 @@ var overviewerConfig = {
                 172
             ], 
             "isOverlay": false, 
-            "last_rendertime": 1409930991, 
+            "last_rendertime": 1409976540, 
             "name": "SW", 
             "poititle": "Markers", 
             "north_direction": 0, 
@@ -32,7 +32,7 @@ var overviewerConfig = {
                 172
             ], 
             "isOverlay": false, 
-            "last_rendertime": 1409930991, 
+            "last_rendertime": 1409976540, 
             "name": "SE", 
             "poititle": "Markers", 
             "north_direction": 1, 
@@ -54,7 +54,7 @@ var overviewerConfig = {
                 172
             ], 
             "isOverlay": false, 
-            "last_rendertime": 1409930991, 
+            "last_rendertime": 1409976540, 
             "name": "NW", 
             "poititle": "Markers", 
             "north_direction": 3, 
@@ -76,7 +76,7 @@ var overviewerConfig = {
                 172
             ], 
             "isOverlay": false, 
-            "last_rendertime": 1409930991, 
+            "last_rendertime": 1409976540, 
             "name": "Nighttime", 
             "poititle": "Markers", 
             "north_direction": 0, 
@@ -126,6 +126,6 @@ var overviewerConfig = {
             67, 
             94
         ], 
-        "cacheTag": "1409931522"
+        "cacheTag": "1409978185"
     }
 };
