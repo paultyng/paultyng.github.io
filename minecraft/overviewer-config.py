93a743865664e0ce952ca232f21eaae9a4aa6e9e
Paul Tyng <paul@paultyng.net>
2014-09-22 16:53:04 -0400

    Update renders, use optipng

--- minecraft/overviewer-config.py
+++ minecraft/overviewer-config.py
@@ -1,32 +1,35 @@
 import sys,os
 
+from optimizeimages import optipng
+
 resources = [Base(), EdgeLines(), MineralOverlay(), SlimeOverlay(), SmoothLighting()]
 
-savesdirectory = "C:\\users\\Paul\\AppData\\Roaming\\.minecraft\\saves"
+savesdirectory = "d:\\minecraft\\game\\saves"
 
 worlddirectory = max([os.path.join(savesdirectory, d) for d in os.listdir(savesdirectory)], key = os.path.getmtime)
 
-sys.stderr.write("Using world %(worlddirectory)s" % locals())
+sys.stderr.write("Using world %(worlddirectory)s\n" % locals())
 
 worlds['My World'] = worlddirectory
 
 outputdir = "D:\\github\\paultyng.github.io\\minecraft"
 
-texturepath = "C:\\Users\\Paul\\AppData\\Roaming\\.minecraft\\versions\\1.8\\1.8.jar"
+texturepath = "D:\\Minecraft\\game\\versions\\1.8\\1.8.jar"
 
 rendermode = "smooth_lighting"
 
-renders["render1"] = {
+renders["osw"] = {
         'world': 'My World',
         'northdirection': 'upper-left',
         'title': 'SW',
+        "optimizeimg": [optipng()],
 }
 
-renders["render2"] = {
-        'world': 'My World',
-        'northdirection': 'upper-right',
-        'title': 'SE',
-}
+#renders["render2"] = {
+#        'world': 'My World',
+#        'northdirection': 'upper-right',
+#        'title': 'SE',
+#}
 
 #renders["render3"] = {
 #        'world': 'My World',
@@ -34,17 +37,17 @@ renders["render2"] = {
 #        'title': 'NE',
 #}
 
-renders["render3"] = {
-        'world': 'My World',
-        'northdirection': 'lower-left',
-        'title': 'NW',
-}
+#renders["render3"] = {
+#        'world': 'My World',
+#        'northdirection': 'lower-left',
+#        'title': 'NW',
+#}
 
-renders["render4"] = {
-        'world': 'My World',
-        'title': 'Nighttime',
-        'rendermode': 'smooth_night',
-}
+#renders["render4"] = {
+#        'world': 'My World',
+#        'title': 'Nighttime',
+#        'rendermode': 'smooth_night',
+#}
 
 #renders["render6"] = {
 #        'world': 'My World',
